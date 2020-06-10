bf238c48372ddec94aa2f4e539e932895e49a146
Paulo Aguiar <paulopma@hotmail.com>
2020-06-10 11:37:37 -0300

    Arquivos de produto detalhes criados

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
+
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+      https://www.figma.com/file/QX53pGgKy1yHjJBs2NkTcb/mello-4Used-2?node-id=0%3A1
       </div>
     )
   }
---
+++ src/components/ProdutoDetalhes/ProdutoDetalhes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class ProdutoDetalhes extends React.Component {
+  state = {
+    productName: ''
+  }
+
+  
+
+  render() {
+    return(
+      <main>
+
+      </main>
+    )
+  }
+}
