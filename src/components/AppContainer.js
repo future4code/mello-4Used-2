dd261993ab1ffcc741f0a40461f639d7ad83c6d4
Pedro <ntourinocp@gmail.com>
2020-06-11 15:14:44 -0300

    Update Componente AppContainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import { Navbar } from "./Navbar";
+import { Home } from "./Home";
+import { Sales } from "./Sales";
+import { Store } from "./Store";
+
+export const ComponentsContainer = styled.div`
+  width: 100%;
+`;
+
+export const ScreenComponents = styled.div`
+  padding: 0 5em;
+`;
 
 export class AppContainer extends Component {
-  render() {
-    return (
-      <div>
+  state = {
+    renderedScreen: "home",
+  };
 
-      </div>
-    )
+  handleHomeLink = () => {
+    this.setState({ renderedScreen: "home" });
+  };
+
+  handleSalesLink = () => {
+    this.setState({ renderedScreen: "sales" });
+  };
+
+  handleStoreLink = () => {
+    this.setState({ renderedScreen: "store" });
+  };
+
+  render() {
+    const rendersScreen = () => {
+      switch (this.state.renderedScreen) {
+        case "home":
+          return (
+            <Home
+              renderSalesScreenProps={this.handleSalesLink}
+              renderStoreScreenProps={this.handleStoreLink}
+            />
+          );
+        case "sales":
+          return <Sales />;
+        case "store":
+          return <Store />;
+        default:
+      }
+    };
+
+    return (
+      <ComponentsContainer>
+        <Navbar
+          renderHomeScreenProps={this.handleHomeLink}
+          renderSalesScreenProps={this.handleSalesLink}
+          renderStoreScreenProps={this.handleStoreLink}
+        />
+        <ScreenComponents>{rendersScreen()}</ScreenComponents>
+      </ComponentsContainer>
+    );
   }
 }
