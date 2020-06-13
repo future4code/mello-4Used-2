9baf5ebe7323131ce61f8d0e0a15f02b51878faa
Lourenço Passos <lo.passos93@gmail.com>
2020-06-12 21:10:04 -0300

    lógica pronta carrinho - sem estilo

--- src/components/ProdutoDetalhes/ProdutoDetalhes.js
+++ src/components/ProdutoDetalhes/ProdutoDetalhes.js
@@ -1,81 +1,91 @@
-import React from 'react'
-import axios from 'axios'
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core'
-import ShoppingCart from '../ShoppingCart'
-import {
-  Main,
-  Product,
-  ButtonAddCart,
-  ButtonBack
-} from './style'
+import React from "react";
+import axios from "axios";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+// import ShoppingCart from '../ShoppingCart'
+import { Main, Product, ButtonAddCart, ButtonBack } from "./style";
 
-const myTheme = createMuiTheme ({
+const myTheme = createMuiTheme({
   palette: {
     primary: {
-      main: '#12C0D2'
+      main: "#12C0D2",
     },
     secondary: {
-      main: '#FFC81A'
-    }
-  }
-})
+      main: "#FFC81A",
+    },
+  },
+});
 
 export default class ProdutoDetalhes extends React.Component {
   state = {
-    productList: '',
-    cartDiv: true
-  }
+    productList: "",
+    cartDiv: false,
+  };
 
   componentDidMount = () => {
-    this.getProduct()
-  }
-  
+    this.getProduct();
+  };
 
   getProduct = (productId) => {
-    productId = this.state.productId
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products')
+    productId = this.state.productId;
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products"
+      )
       .then((response) => {
-        this.setState({productList: response.data.products})
+        this.setState({ productList: response.data.products });
         this.state.productList.map((product) => {
           product.id === productId &&
-          this.setState({
-            productName: product.name, 
-            productImages: product.photos,
-            productDescription: product.description,
-            productPrice: product.price
-          })
-        })
-      }) 
-      .catch((error) => {
-        window.alert(error)
+            this.setState({
+              productName: product.name,
+              productImages: product.photos,
+              productDescription: product.description,
+              productPrice: product.price,
+              productId: product.id,
+            });
+        });
       })
-  }
-
-  onClickAddToCart = () => {
-
-  }
+      .catch((error) => {
+        window.alert(error);
+      });
+  };
 
   render() {
-    console.log(this.state.productImgs)
-    return(
+    return (
       <MuiThemeProvider theme={myTheme}>
-      <Main>
-        {this.state.cartDiv ?
-        <ShoppingCart /> :
-        <div>
-          <Product>
-            <img alt='produto' src={this.props.productImages} />
+        <Main>
+          {this.state.cartDiv ? (
+            <div />
+          ) : (
             <div>
-              <h2>{this.props.productName}</h2>
-              <p>{this.props.productDescription}</p>
-              <p>R$ {this.props.productPrice}</p>
+              <Product>
+                <img alt="produto" src={this.props.productImages} />
+                <div>
+                  <h2>{this.props.productName}</h2>
+                  <p>{this.props.productDescription}</p>
+                  <p>{this.props.productId}</p>
+                </div>
+              </Product>
+              <ButtonAddCart
+                onClick={() =>
+                  this.props.onClickAddToCart(this.props.productName)
+                }
+                size="medium"
+                variant="extendedFab"
+                color="secondary"
+              >
+                colocar no carrinho
+              </ButtonAddCart>
+              <ButtonBack
+                onClick={this.props.onClickBack}
+                variant="text"
+                color="primary"
+              >
+                escolher mais produtos
+              </ButtonBack>
             </div>
-          </Product>
-          <ButtonAddCart onCLick={this.onClickAddToCart} size='medium' variant='extendedFab' color='secondary'>colocar no carrinho</ButtonAddCart>
-          <ButtonBack onClick={this.props.onClickBack} variant='text' color='primary'>escolher mais produtos</ButtonBack>
-        </div>}
-      </Main>
+          )}
+        </Main>
       </MuiThemeProvider>
-    )
+    );
   }
 }
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import axios from 'axios'
+import axios from "axios";
 import { Card } from "./Card";
 import ProdutoDetalhes from "./ProdutoDetalhes/ProdutoDetalhes";
 
@@ -21,53 +21,70 @@ export class Store extends React.Component {
   state = {
     productDetailsDiv: false,
     productList: [],
-    productName: '', 
-    productImages: '',
-    productDescription: '',
-    productPrice: '',
-    productId: ''
+    productName: "",
+    productImages: "",
+    productDescription: "",
+    productPrice: "",
+    productId: "",
+    productsInCart: "",
+    cartList: [],
   };
 
   componentDidMount = () => {
-    this.getProductList()
-  }
+    this.getProductList();
+  };
+
+  onClickAddToCart = (name) => {
+    let productToCart = this.state.productList.find((product) => {
+      return product.name === name;
+    });
+
+    this.setState({ cartList: [...this.state.cartList, productToCart] });
+    console.log(this.state.cartList);
+  };
 
   getProductList = () => {
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products')
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products"
+      )
       .then((response) => {
-        this.setState({productList: response.data.products})
+        this.setState({ productList: response.data.products });
       })
       .catch((error) => {
-        window.alert(error)
-      })
-  }
+        window.alert(error);
+      });
+  };
 
   getProductById = (productId) => {
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products')
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products"
+      )
       .then((response) => {
-        this.setState({productList: response.data.products})
+        this.setState({ productList: response.data.products });
         this.state.productList.map((product) => {
           product.id === productId &&
-          this.setState({
-            productName: product.name, 
-            productImages: product.photos,
-            productDescription: product.description,
-            productPrice: product.price
-          })
-        })
-        this.changeRender()
-      }) 
-      .catch((error) => {
-        window.alert(error)
+            this.setState({
+              productName: product.name,
+              productImages: product.photos,
+              productDescription: product.description,
+              productPrice: product.price,
+            });
+        });
+        this.changeRender();
       })
-  }
+      .catch((error) => {
+        window.alert(error);
+      });
+  };
 
   changeRender = () => {
-    this.setState({productDetailsDiv: !this.state.productDetailsDiv})
-  }
+    this.setState({ productDetailsDiv: !this.state.productDetailsDiv });
+  };
 
   render() {
-    console.log(this.state.productList)
+    console.log(this.state.cartList);
     const renderedProductList = this.state.productList.map((product) => {
       return (
         <div onClick={() => this.getProductById(product.id)}>
@@ -85,17 +102,33 @@ export class Store extends React.Component {
 
     return (
       <div>
-        {this.state.productDetailsDiv ?
-        <ProdutoDetalhes 
-          productName={this.state.productName}
-          productPrice={this.state.productPrice}
-          productDescription={this.state.productDescription}
-          productImages={this.state.productImages}
-          onClickBack={this.changeRender}
-        /> :
-        <StoreContainer>
-          <ListContainer >{renderedProductList}</ListContainer>
-        </StoreContainer>}
+        {this.state.productDetailsDiv ? (
+          <ProdutoDetalhes
+            productName={this.state.productName}
+            productPrice={this.state.productPrice}
+            productDescription={this.state.productDescription}
+            productImages={this.state.productImages}
+            onClickBack={this.changeRender}
+            onClickAddToCart={this.onClickAddToCart}
+          />
+        ) : (
+          <StoreContainer>
+            <ListContainer>{renderedProductList}</ListContainer>
+            {this.state.cartList.map((products) => {
+              return (
+                <div>
+                  <p>{products.name}</p>
+                  <p>{this.state.cartList.lenght}</p>
+                </div>
+              );
+            })}
+            <button>Ver Carrinho</button>
+            <div>
+              <h1>Carrinho</h1>
+              <div></div>
+            </div>
+          </StoreContainer>
+        )}
       </div>
     );
   }
